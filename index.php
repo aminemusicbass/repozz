3d2ecef6d53b0dd10cab1476234777ba7a0a7672
aminemusicbass <aminemusicbass@gmail.com>
2024-11-30 15:37:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ $parameter = 2; // 1 => Both , 2 => Country , 3 => Proxy , 4 => Simple
 $wordpress = true; // Allow Wordpress Include
 $checkfileup = false; // Check Link UP
 
-$country = array("ma","au","nz"); #Country's Allowed To Access
+$country = array("ma","au","nz","br","pt","es"); #Country's Allowed To Access
 
 /************************************************************* PARAMETRES ******************************************************************/
 
